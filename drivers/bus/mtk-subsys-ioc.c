46b0a340b4d3ea8a1cf9cbe8b8098489de9a2654
Andrey Zolotarev <a.zolotarev@ndmsystems.com>
2019-10-21 03:31:54 +0700

    mtk-subsys-ioc: control ARM CoreLink CCI-400 slave interface S4
    CCI - CacheCoherent Interconnect.
    
    MT7622 need enable snoop and DVM requests for CCI-400 slave interface S4 on:
     - ethdmasys IOC coherence path enabled (Frame Engine)
     - wbhif IOC coherence path enabled (Wireless)
    
    Otherwise DMA coherence is broken.
    
    MTK SDK direct control CCI-400 S4 via raeth. This is ugly solution.

--- drivers/bus/mtk-subsys-ioc.c
+++ drivers/bus/mtk-subsys-ioc.c
@@ -16,10 +16,19 @@
 #include <linux/mfd/syscon.h>
 #include <linux/of.h>
 #include <linux/of_platform.h>
+#include <linux/of_address.h>
+#include <linux/io.h>
 #include <linux/regmap.h>
 
+#define CCI400_S4_OF_NODE		"arm,cci-400-s4"
+#define CCI400_SCR_OFFS			0x000
+#define CCI400_SCR_SNOOP		BIT(0)
+#define CCI400_SCR_DVM			BIT(1)
+
+#define WBSYS_OF_NODE			"mediatek,wbsys"
 #define WBSYS_IOC_PDMA			BIT(31)
 
+#define HIFSYS_OF_NODE			"mediatek,hifsys"
 #define HIFSYS_IOC_PCIE0		BIT(0)
 #define HIFSYS_IOC_PCIE1_SATA		BIT(1)
 #define HIFSYS_IOC_PCIE_SATA_MASK	GENMASK(1, 0)
@@ -29,6 +38,7 @@
 #define HIFSYS_IOC_SSUSB_DEV		BIT(4)
 #define HIFSYS_IOC_SSUSB_MASK		GENMASK(4, 2)
 
+#define ETHSYS_OF_NODE			"mediatek,ethsys"
 #define ETHSYS_IOC_PDMA			BIT(0)
 #define ETHSYS_IOC_QDMA			BIT(1)
 #define ETHSYS_IOC_PPE			BIT(2)
@@ -48,63 +58,100 @@ struct mtk_subsys_ioc_pdata {
 	unsigned int eth_ioc_off;
 };
 
+static int arm_cci400_slave_snoop(struct device_node *np, const char *sp)
+{
+	struct device_node *sl_np;
+	void __iomem *sl_base;
+	u32 reg_val;
+
+	sl_np = of_parse_phandle(np, sp, 0);
+	if (!sl_np)
+		return -ENODEV;
+
+	sl_base = of_iomap(sl_np, 0);
+	of_node_put(sl_np);
+
+	if (!sl_base)
+		return -ENOMEM;
+
+	reg_val = __raw_readl(sl_base + CCI400_SCR_OFFS);
+	if (!(reg_val & CCI400_SCR_DVM) ||
+	    !(reg_val & CCI400_SCR_SNOOP)) {
+		reg_val |= (CCI400_SCR_DVM | CCI400_SCR_SNOOP);
+		__raw_writel(reg_val, sl_base + CCI400_SCR_OFFS);
+	}
+
+	iounmap(sl_base);
+
+	return 0;
+}
+
 static int mtk_subsys_ioc_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	struct device_node *np = dev->of_node;
 	const struct mtk_subsys_ioc_pdata *data;
 	struct regmap *wbsys, *hifsys, *ethsys;
+	bool is_wbsys_ioc = false;
+	bool is_ethsys_ioc = false;
 
 	data = of_device_get_match_data(dev);
 	if (!data)
 		return -ENODEV;
 
-	/* enable WIFI IOC if needed */
-	if (data->has_wbsys && of_property_read_bool(np, "mediatek,en_wifi")) {
-		wbsys = syscon_regmap_lookup_by_phandle(np, "mediatek,wbsys");
-		if (IS_ERR(wbsys))
-			return PTR_ERR(wbsys);
+	if (data->has_wbsys)
+		is_wbsys_ioc = of_property_read_bool(np, "mediatek,en_wifi");
 
-		regmap_update_bits(wbsys, data->wb_ioc_off, WBSYS_IOC_PDMA,
-				   WBSYS_IOC_PDMA);
+	if (data->has_ethsys)
+		is_ethsys_ioc = of_property_read_bool(np, "mediatek,en_eth");
+
+	/* enable Snoop & DVM message requests on ARM CCI slave interface 4 */
+	if (is_wbsys_ioc || is_ethsys_ioc)
+		arm_cci400_slave_snoop(np, CCI400_S4_OF_NODE);
+
+	/* enable wbsys IOC if needed */
+	if (is_wbsys_ioc) {
+		wbsys = syscon_regmap_lookup_by_phandle(np, WBSYS_OF_NODE);
+		if (!IS_ERR(wbsys)) {
+			regmap_update_bits(wbsys, data->wb_ioc_off,
+					   WBSYS_IOC_PDMA,
+					   WBSYS_IOC_PDMA);
+		}
+	}
+
+	/* enable ethernet IOC if needed */
+	if (is_ethsys_ioc) {
+		ethsys = syscon_regmap_lookup_by_phandle(np, ETHSYS_OF_NODE);
+		if (!IS_ERR(ethsys)) {
+			unsigned int reg_val = ETHSYS_IOC_PDMA;
+
+			/* hwnat HWFQ path is SW coherence only */
+			if (!IS_ENABLED(CONFIG_RA_HW_NAT_QDMA))
+				reg_val |= ETHSYS_IOC_QDMA;
+
+			regmap_update_bits(ethsys, data->eth_ioc_off,
+					   ETHSYS_IOC_ETH_MASK,
+					   reg_val);
+		}
 	}
 
 	/* enable USB/PCIe/SATA IOC if needed */
 	if (data->has_hifsys) {
-		hifsys = syscon_regmap_lookup_by_phandle(np, "mediatek,hifsys");
-		if (IS_ERR(hifsys))
-			return PTR_ERR(hifsys);
+		hifsys = syscon_regmap_lookup_by_phandle(np, HIFSYS_OF_NODE);
+		if (!IS_ERR(hifsys)) {
+			if (of_property_read_bool(np, "mediatek,en_usb"))
+				regmap_update_bits(hifsys, data->hif_ioc_off,
+						   HIFSYS_IOC_SSUSB_MASK,
+						   HIFSYS_IOC_SSUSB_XHCI_MAS |
+						   HIFSYS_IOC_SSUSB_XHCI_U3DATA |
+						   HIFSYS_IOC_SSUSB_DEV);
 
-		if (of_property_read_bool(np, "mediatek,en_usb"))
-			regmap_update_bits(hifsys, data->hif_ioc_off,
-					   HIFSYS_IOC_SSUSB_MASK,
-					   HIFSYS_IOC_SSUSB_XHCI_MAS |
-					   HIFSYS_IOC_SSUSB_XHCI_U3DATA |
-					   HIFSYS_IOC_SSUSB_DEV);
-
-		if (of_property_read_bool(np, "mediatek,en_pcie_sata"))
-			regmap_update_bits(hifsys, data->hif_ioc_off,
-					   HIFSYS_IOC_PCIE_SATA_MASK,
-					   HIFSYS_IOC_PCIE0 |
-					   HIFSYS_IOC_PCIE1_SATA);
-	}
-
-	/* enable ethernet IOC if needed */
-	if (data->has_ethsys && of_property_read_bool(np, "mediatek,en_eth")) {
-		ethsys = syscon_regmap_lookup_by_phandle(np, "mediatek,ethsys");
-		if (IS_ERR(ethsys))
-			return PTR_ERR(ethsys);
-
-		/* check QoS */
-		if (IS_ENABLED(CONFIG_QDMA_SUPPORT_QOS))
-			regmap_update_bits(ethsys, data->eth_ioc_off,
-					   ETHSYS_IOC_ETH_MASK,
-					   ETHSYS_IOC_PDMA);
-		else
-			regmap_update_bits(ethsys, data->eth_ioc_off,
-					   ETHSYS_IOC_ETH_MASK,
-					   ETHSYS_IOC_PDMA |
-					   ETHSYS_IOC_QDMA);
+			if (of_property_read_bool(np, "mediatek,en_pcie_sata"))
+				regmap_update_bits(hifsys, data->hif_ioc_off,
+						   HIFSYS_IOC_PCIE_SATA_MASK,
+						   HIFSYS_IOC_PCIE0 |
+						   HIFSYS_IOC_PCIE1_SATA);
+		}
 	}
 
 	return 0;

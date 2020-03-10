3033e6e943fbf49fc99d687f80481e6e94d6afec
Yangtao Li <tiny.windzz@gmail.com>
2020-03-10 13:22:46 +0000

    nvmem: core: add nvmem_cell_read_u64
    Add nvmem_cell_read_u64() helper to ease read of an u64 value on consumer
    side. This helper is useful on some sunxi platform that has 64 bits data
    cells stored in no volatile memory.
    
    Signed-off-by: Yangtao Li <tiny.windzz@gmail.com>
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Link: https://lore.kernel.org/r/20200310132257.23358-4-srinivas.kandagatla@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/nvmem-consumer.h
+++ include/linux/nvmem-consumer.h
@@ -37,6 +37,7 @@ void *nvmem_cell_read(struct nvmem_cell *cell, size_t *len);
 int nvmem_cell_write(struct nvmem_cell *cell, void *buf, size_t len);
 int nvmem_cell_read_u16(struct device *dev, const char *cell_id, u16 *val);
 int nvmem_cell_read_u32(struct device *dev, const char *cell_id, u32 *val);
+int nvmem_cell_read_u64(struct device *dev, const char *cell_id, u64 *val);
 
 /* direct nvmem device read/write interface */
 struct nvmem_device *nvmem_device_get(struct device *dev, const char *name);
@@ -95,6 +96,12 @@ static inline int nvmem_cell_read_u16(struct device *dev,
 
 static inline int nvmem_cell_read_u32(struct device *dev,
 				      const char *cell_id, u32 *val)
+{
+	return -ENOSYS;
+}
+
+static inline int nvmem_cell_read_u64(struct device *dev,
+				      const char *cell_id, u64 *val)
 {
 	return -ENOSYS;
 }

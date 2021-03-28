b1bd89af780f9c3592ad6a0a2c5d61e8bf342ee3
Andrey Zolotarev <a.zolotarev@ndmsystems.com>
2021-03-28 23:11:40 +0700

    L2TP: avoid realloc skb headroom on pppol2tp_xmit, NDMS-1070
    Improve performance.
    
    Porting optimizations from kernel-3.4.

--- include/linux/l2tp.h
+++ include/linux/l2tp.h
@@ -10,4 +10,11 @@
 #include <linux/in6.h>
 #include <uapi/linux/l2tp.h>
 
+/* tune skb pad for CPU with L1_CACHE_BYTES >= 128 (e.g. MT7622) */
+#if L1_CACHE_BYTES > 64
+#define L2TP_SKB_PAD	(NET_SKB_PAD >> 1)
+#else
+#define L2TP_SKB_PAD	NET_SKB_PAD_ORIG
+#endif
+
 #endif

74b924b5cafcf20207733e5bc1e7404bc9f201f7
Douglas Anderson <dianders@chromium.org>
2021-09-13 17:05:51 +0100

    nvmem: core: Add stubs for nvmem_cell_read_variable_le_u32/64 if !CONFIG_NVMEM
    When I added nvmem_cell_read_variable_le_u32() and
    nvmem_cell_read_variable_le_u64() I forgot to add the "static inline"
    stub functions for when CONFIG_NVMEM wasn't defined. Add them
    now. This was causing problems with randconfig builds that compiled
    `drivers/soc/qcom/cpr.c`.
    
    Fixes: 6feba6a62c57 ("PM: AVS: qcom-cpr: Use nvmem_cell_read_variable_le_u32()")
    Fixes: a28e824fb827 ("nvmem: core: Add functions to make number reading easy")
    Reported-by: kernel test robot <lkp@intel.com>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Link: https://lore.kernel.org/r/20210913160551.12907-1-srinivas.kandagatla@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/nvmem-consumer.h
+++ include/linux/nvmem-consumer.h
@@ -111,6 +111,20 @@ static inline int nvmem_cell_read_u64(struct device *dev,
 	return -ENOSYS;
 }
 
+static inline int nvmem_cell_read_variable_le_u32(struct device *dev,
+						 const char *cell_id,
+						 u32 *val)
+{
+	return -ENOSYS;
+}
+
+static inline int nvmem_cell_read_variable_le_u64(struct device *dev,
+						  const char *cell_id,
+						  u64 *val)
+{
+	return -ENOSYS;
+}
+
 static inline struct nvmem_device *nvmem_device_get(struct device *dev,
 						    const char *name)
 {

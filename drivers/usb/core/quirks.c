d36bf40c3c011eb32e36d31e783ea2f9582f1d85
Vlad Starodubtsev <vlad.starodubtsev@keenetic.com>
2022-11-10 16:41:09 +0300

    USB: add quirk for devices with broken LANG ID, NDM-2419
     * apply this quirk for Brovi E3372-325

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -355,6 +355,9 @@ static const struct usb_device_id usb_quirk_list[] = {
 	/* Yota W0 */
 	{ USB_DEVICE(0x1286, 0x4e31), .driver_info = USB_QUIRK_DELAY_CTRL_MSG },
 
+	/* Brovi E3372-325 */
+	{ USB_DEVICE(0x3566, 0x2001), .driver_info = USB_QUIRK_LANG_ID },
+
 	{ }  /* terminating entry must be last */
 };
 

122ceda66937fc8bee935db720f610876eb37e42
Andrey Zolotarev <andrey.zolotarev@keenetic.com>
2023-01-27 04:13:11 +0700

    mtd: spinand: macronix: suppress MX35LF1GE4AB warning
    Porting from MTK code.

--- drivers/mtd/nand/spi/macronix.c
+++ drivers/mtd/nand/spi/macronix.c
@@ -86,7 +86,7 @@ static int mx35lf1ge4ab_ecc_get_status(struct spinand_device *spinand,
 		if (mx35lf1ge4ab_get_eccsr(spinand, &eccsr))
 			return nand->eccreq.strength;
 
-		if (WARN_ON(eccsr > nand->eccreq.strength || !eccsr))
+		if (eccsr > nand->eccreq.strength || !eccsr)
 			return nand->eccreq.strength;
 
 		return eccsr;

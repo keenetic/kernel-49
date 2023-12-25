91bd1945aabfa5c27db47f9d046b5a7ba6c8cef1
Markov Mikhail <mikhail.markov@keenetic.com>
2023-12-25 11:17:45 +0300

    ndm: update WARP header to comply with V3, SYS-1067

--- ndm/warp/warp_wifi.h
+++ ndm/warp/warp_wifi.h
@@ -7,11 +7,18 @@
 #define WIFI_ERR_RECOV_STOP_PDMA0 		1
 #define WIFI_ERR_RECOV_RESET_PDMA0 		2
 #define WIFI_ERR_RECOV_STOP_IDLE_DONE 		3
+#define WIFI_ETH_ERR_STOP_WED_RX_TRAFFIC	4
+#define WIFI_ETH_ERR_START_WED_RX_TRAFFIC	5
 
 enum {
 	BUS_TYPE_PCIE,
 	BUS_TYPE_AXI,
 	BUS_TYPE_MAX
+};
+
+enum {
+	WIFI_FREE_IRQ = 0,
+	WIFI_REQUEST_IRQ = 1,
 };
 
 enum {
@@ -54,6 +61,7 @@ struct ring_ctrl {
 	u32 cnt;
 	u32 cidx;
 	u32 didx;
+	u32 lens;
 	dma_addr_t cb_alloc_pa;
 };
 
@@ -75,6 +83,7 @@ struct wifi_hw {
 	u32 chip_id;
 	u32 tx_ring_num;
 	u32 tx_token_nums;
+	u32 tx_buf_nums;
 	u32 sw_tx_token_nums;
 	u32 hw_rx_token_num;
 	u32 rx_page_nums;
@@ -100,6 +109,7 @@ struct wifi_hw {
 	u32 rx_pkt_size;
 	u16 rx_page_size;
 	u32 max_rxd_size;
+	u32 sign_base_cr;
 	u8 rx_rro_data_ring_num;
 	u8 rx_rro_page_ring_num;
 	u8 rx_rro_ind_cmd_ring_num;
@@ -110,9 +120,6 @@ struct wifi_hw {
 	struct ring_ctrl rx_rro_ind_cmd;
 	struct rro_ctrl rro_ctl;
 
-	bool msi_enable;
-	u32 hif_type;
-	u32 irq;
 	unsigned long wpdma_base;
 	unsigned long base_phy_addr;
 	unsigned long base_addr;
@@ -121,17 +128,39 @@ struct wifi_hw {
 	u8 wfdma_tx_done_trig1_bit;
 	u8 wfdma_tx_done_free_notify_trig_bit;
 	u8 wfdma_rx_done_trig0_bit;
+	u8 wfdma_rx_done_trig0_enable;
 	u8 wfdma_rx_done_trig1_bit;
+	u8 wfdma_rx_done_trig1_enable;
 	u8 wfdma_rro_rx_done_trig0_bit;
 	u8 wfdma_rro_rx_done_trig1_bit;
 	u8 wfdma_rro_rx_pg_trig0_bit;
 	u8 wfdma_rro_rx_pg_trig1_bit;
 	u8 wfdma_rro_rx_pg_trig2_bit;
-	bool dbdc_mode;
-	u8 max_amsdu_nums;
-	u32 max_amsdu_len;
-	u8 mac_ver;
+
+	u32 hif_type;
+	u32 irq;
 	u32 hw_cap;
+	u32 max_amsdu_len;
+	u32 max_wcid_nums;
+	u32 wed_v1_compatible_en_addr;
+	u32 wed_v1_compatible_en_msk;
+	u32 wed_v1_compatible_tx0_addr;
+	u32 wed_v1_compatible_tx0_msk;
+	u32 wed_v1_compatible_tx0_id;
+	u32 wed_v1_compatible_tx1_addr;
+	u32 wed_v1_compatible_tx1_msk;
+	u32 wed_v1_compatible_tx1_id;
+	u32 wed_v1_compatible_rx1_addr;
+	u32 wed_v1_compatible_rx1_msk;
+	u32 wed_v1_compatible_rx1_id;
+
+	u8 max_amsdu_nums;
+	u8 mac_ver;
+	u8 src;
+	bool msi_enable;
+	bool dbdc_mode;
+	bool rm_vlan;
+	bool hdtr_mode;
 };
 
 struct wifi_ops {
@@ -150,6 +179,10 @@ struct wifi_ops {
 	void (*rxinfo_wrapper)(u8 *rx_info, struct wlan_rx_info *info);
 	void (*do_wifi_reset)(void *priv_data);
 	void (*update_wo_rxcnt)(void *priv_data, struct wo_cmd_rxcnt_t *wo_rxcnt, u8 num);
+	void (*request_irq)(void *priv_data, u32 irq);
+	void (*free_irq)(void *priv_data, u32 irq);
+	void (*hb_check_notify)(void *priv_data);
+	void (*fbuf_v1_init)(u8 *fbuf, dma_addr_t pkt_pa, u32 tkid, u8 src);
 };
 
 #endif /*_WARP_WIFI_COMMON_H_*/

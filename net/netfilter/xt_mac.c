68ab3d38907dfa41330ab4b47f267cf6d2c1b8cb
Ilia Ponetaev <ilia.ponetaev@keenetic.com>
2024-02-02 14:27:05 +0300

    xt_mac.c: add support for ntc label (NDM-3127)

--- net/netfilter/xt_mac.c
+++ net/netfilter/xt_mac.c
@@ -19,13 +19,15 @@
 #include <linux/netfilter/xt_mac.h>
 #include <linux/netfilter/x_tables.h>
 
+#include <linux/ntc_shaper_hooks.h>
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Netfilter Core Team <coreteam@netfilter.org>");
 MODULE_DESCRIPTION("Xtables: MAC address match");
 MODULE_ALIAS("ipt_mac");
 MODULE_ALIAS("ip6t_mac");
 
-static bool mac_mt(const struct sk_buff *skb, struct xt_action_param *par)
+static bool mac_mtv0(const struct sk_buff *skb, struct xt_action_param *par)
 {
 	const struct xt_mac_info *info = par->matchinfo;
 	bool ret;
@@ -41,25 +43,79 @@ static bool mac_mt(const struct sk_buff *skb, struct xt_action_param *par)
 	return ret;
 }
 
-static struct xt_match mac_mt_reg __read_mostly = {
-	.name      = "mac",
-	.revision  = 0,
-	.family    = NFPROTO_UNSPEC,
-	.match     = mac_mt,
-	.matchsize = sizeof(struct xt_mac_info),
-	.hooks     = (1 << NF_INET_PRE_ROUTING) | (1 << NF_INET_LOCAL_IN) |
-	             (1 << NF_INET_FORWARD),
-	.me        = THIS_MODULE,
+static bool mac_match_ct(const struct sk_buff *skb, struct xt_action_param *par)
+{
+	const struct xt_mac_info *info = par->matchinfo;
+	enum ip_conntrack_info ctinfo;
+	const struct nf_conn *ct;
+	struct nf_ct_ext_ntc_label *ntc;
+	bool ret;
+
+	ct = nf_ct_get(skb, &ctinfo);
+	if (ct == NULL || nf_ct_is_untracked(ct))
+		return false;
+
+	ntc = nf_ct_ext_find_ntc(ct);
+	if (ntc == NULL || !nf_ct_ext_ntc_filled(ntc) || !nf_ct_ext_ntc_mac_isset(ntc))
+		return false;
+
+	ret  = ether_addr_equal(ntc->mac, info->srcaddr);
+	ret ^= info->invert;
+	return ret;
+}
+
+static bool mac_mtv1(const struct sk_buff *skb, struct xt_action_param *par)
+{
+	const struct xt_mac_info *info = par->matchinfo;
+	bool ret;
+
+	if (mac_match_ct(skb, par))
+		return true;
+
+	if (par->hooknum == NF_INET_POST_ROUTING)
+		return false;
+	if (skb->dev == NULL || skb->dev->type != ARPHRD_ETHER)
+		return false;
+	if (skb_mac_header(skb) < skb->head)
+		return false;
+	if (skb_mac_header(skb) + ETH_HLEN > skb->data)
+		return false;
+	ret  = ether_addr_equal(eth_hdr(skb)->h_source, info->srcaddr);
+	ret ^= info->invert;
+	return ret;
+}
+
+static struct xt_match mac_mt_reg[] __read_mostly = {
+	{
+		.name      = "mac",
+		.revision  = 0,
+		.family    = NFPROTO_UNSPEC,
+		.match     = mac_mtv0,
+		.matchsize = sizeof(struct xt_mac_info),
+		.hooks     = (1 << NF_INET_PRE_ROUTING) | (1 << NF_INET_LOCAL_IN) |
+					 (1 << NF_INET_FORWARD),
+		.me        = THIS_MODULE,
+	},
+	{
+		.name      = "mac",
+		.revision  = 1,
+		.family    = NFPROTO_UNSPEC,
+		.match     = mac_mtv1,
+		.matchsize = sizeof(struct xt_mac_info),
+		.hooks     = (1 << NF_INET_PRE_ROUTING) | (1 << NF_INET_LOCAL_IN) |
+					 (1 << NF_INET_FORWARD) | (1 << NF_INET_POST_ROUTING),
+		.me        = THIS_MODULE,
+	},
 };
 
 static int __init mac_mt_init(void)
 {
-	return xt_register_match(&mac_mt_reg);
+	return xt_register_matches(mac_mt_reg, ARRAY_SIZE(mac_mt_reg));
 }
 
 static void __exit mac_mt_exit(void)
 {
-	xt_unregister_match(&mac_mt_reg);
+	xt_unregister_matches(mac_mt_reg, ARRAY_SIZE(mac_mt_reg));
 }
 
 module_init(mac_mt_init);
